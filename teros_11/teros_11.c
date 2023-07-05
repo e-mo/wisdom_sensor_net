0fc0a0fdde25b1d8100183d566c7be2a1d04d200
amelia vlahogiannis <amelia@ag2v.com>
2023-07-05 09:45:05 -0400

    teros sensor changes

--- teros_11/teros_11.c
+++ teros_11/teros_11.c
@@ -1,7 +1,8 @@
 /*
  * teros_11.c
- * 2023-05-29
+ * 2023-06-09
  * amelia vlahogiannis
+ * amelia@ag2v.com
  */
 
 #include "teros_11.h"
@@ -18,6 +19,7 @@ teros_return teros_init(
 
 	*teros = malloc(sizeof(teros));
 
+	if(teros == NULL) return malloc_null;
 	//save some struct elements to reference in other functions
 	(*teros)->serial = serial;
 	(*teros)->model = model;
@@ -28,10 +30,12 @@ teros_return teros_init(
 
 	uart_init(serial, TEROS_BAUD_RATE);
 
-	gpio_set_function(uart_tx_pin, GPIO_FUNC_UART);
+	gpio_set_function(uart_rx_pin, GPIO_FUNC_UART);
 	//gpio_set_function(uart_tx_pin, GPIO_FUNC_UART);
 	gpio_init(pwr_pin);
 	gpio_set_dir(pwr_pin, GPIO_OUT);
+	
+	return ok;
 }
 
 //grab and return all of the data
@@ -39,81 +43,148 @@ teros_return teros_get_data(teros *teros, teros_data *data) {
 	teros_return status;
 	unsigned char ch;
 	unsigned char str[16];
-	int i = 0;
+	unsigned char vwc_str[8];
+	unsigned char temp_str[8];
+	unsigned char cond_str[16];
+	unsigned char type;
+	unsigned char check;
+	unsigned char crc;
+	double vwc_raw;
+	double temp_raw;
+	double cond_raw;
+	int i, n;
 
-	printf("get data function start \n");
-	
-	gpio_put(teros->pwr_pin, 1);
+	str[0] = '\0';	
+	gpio_put(teros->pwr_pin, 1); //turn it on
 
-	printf("gpio on\n");
-	
-	if(!uart_is_enabled(teros->serial)) return uart_not_enabled;
+	//wait for the uart to fill, time out if it doesnt after 150ms
+	for(i = 0; !uart_is_readable(teros->serial) && i < 150; i += 10) sleep_ms(10);
+	if(i > 140) return timed_out; //time out after 150ms
 
-	sleep_ms(125);
-
-	printf("uart is enabled\n");
-
-	if(uart_is_readable_within_us(teros->serial, 150000)) {
-		i = 0;
-		while(uart_is_readable(uart0)) {
-				str[i] = uart_getc(uart0);
-				i++;
-				}
-		str[i] = '\0';
-		printf("%s\n", str);
-		printf("reading complete\n");
+	for(i = 0; uart_is_readable_within_us(teros->serial, 30000);) { //not sure why it has to be within_us
+									//but it wont work otherwise, or with 
+									//less than 30000
+		ch = uart_getc(teros->serial);
+		if(isprint(ch) || ch == '\t' || ch == '\r') { //leave the tab/cr in for easier processing later
+			str[i] = ch;
+			i++;
 		}
+	}
+
+	gpio_put(teros->pwr_pin, 0); //turn it off
+
+	str[i] = '\0'; //terminate the string
+	if(str[0] == '\0') return read_unsuccessful; //make sure it isnt zero length(this should not be possible)
 	
-	else printf("could not read within 150ms\n");
+	if(str[0] == '\t') { //just be absolutely certain its correct and starts with a tab
+		for(i = 1; str[i] != ' '; i++) { //pull calibrated counts vwc out
+			vwc_str[i - 1] = str[i];
+		} vwc_raw = atof(&vwc_str); //convert string to float
+		i++; //skip over a space
+
+		for(n = 0; str[i] != ' ' && str[i] != '\r'; i++, n++) { //pull temperature
+			temp_str[n] = str[i];
+		} temp_raw = atof(&temp_str);
+		i++; //skip over a space or carriage return
+
+		if(teros->model == teros_12) { //pull conductivity if applicable(teros 12)
+			for(n = 0; str[i] != ' ' && str[i] != '\r'; i++, n++) {
+				cond_str[n] = str[i];
+			} if(teros->model == teros_12) cond_raw = atof(&cond_str);
+			i++;
+		}
+
+		type = str[i]; //sensor type
+		i++;
+		check = str[i]; //checksum
+		i++;
+		crc = str[i]; //crc6
+	}	
+	else return read_unsuccessful; //if its not a tab, bad read
+
+	if(crc != _crc6_validate(&str)) return invalid_check_crc; //validate the checksum of the string
+	if(check != _checksum_validate(&str)) return invalid_check_crc; //same with crc6
 	
-	gpio_put(teros->pwr_pin, 0);
+	//time to convert raw values to standard units
+	if(teros->substrate_type == mineral) data->vwc = _raw_to_m3m3_mineral(vwc_raw);
+	else if(teros->substrate_type == soilless) data->vwc = _raw_to_m3m3_soilless(vwc_raw);
 
-	sleep_ms(3000);
+	data->temperature = temp_raw;
 
-	return 0;
-}
+	data->sensor_type = type;
+	data->checksum = check;
+	data->crc = crc;
 
-//get checksum
-teros_return teros_get_checksum(teros *teros, uint8_t *checksum) {
-	gpio_put(teros->pwr_pin, 1); //sensor will automatically spit out data over uart when powered on
-				     //the pico's digital pins can supply the necessary 3.6mA
-}
+	/*
+	printf("\nWater Content(m^3/m^3): %.6f\n", data->vwc);
+	printf("Temperature(C): %.1f\n", data->temperature);
+	printf("Sensor type: %c\n", type);
+	printf("Checksum:    %c\n", check);
+	printf("CRC:         %c\n", crc);
+	*/
 
-//get crc
-teros_return teros_get_crc(teros *teros, uint8_t *crc) {
-}
-
-//get sensor type
-teros_return teros_get_sensor_type(teros *teros, uint8_t *type) {
-}
-
-//get vwc in m3/m3
-teros_return teros_get_vwc(teros *teros, float *vwc) {
-}
-
-//get vwc raw
-teros_return teros_get_vwc_raw(teros *teros, float *vwc) {
-}
-
-//get dielectric permittivity - teros 12 only
-teros_return teros_get_permittivity(teros *teros, float *permittivity) {
-}
-
-//get temp in c
-teros_return teros_get_tempc(teros *teros, float *temp) {
+	return ok;
 }
 
 //convert raw to m3/m3 for mineral soils
-float raw_to_m3m3_mineral(float raw) {
-	float vwc;
-	vwc = 0.0003879 * raw - 0.6956;
+double _raw_to_m3m3_mineral(double raw) {
+	double vwc;
+	vwc = (0.0003879 * raw) - 0.6956;
 	return vwc;
 }
 
 //convert raw to m3/m3 for soilless media
-float raw_to_m3m3_soilless(float raw) {
-	float vwc;
+double _raw_to_m3m3_soilless(double raw) {
+	double vwc;
 	vwc = 0.0000000006771 * (raw * raw * raw) - 0.000005105 * (raw * raw) + 0.01302 * raw - 10.848;
 	return vwc;
 }
 
+
+
+unsigned char _crc6_validate(const char *buffer) {
+	//this function taken from the teros 11/12 integrator guide
+
+	uint16_t byte;
+	uint16_t i;
+	uint16_t bytes;
+	uint8_t bit;
+	uint8_t crc = 0xfc;
+
+	bytes = strlen(buffer);
+
+	for(byte = 0; byte < bytes; byte++) {
+
+		crc ^= buffer[byte];
+
+		for(bit = 8; bit > 0; bit--) {
+
+			if(crc & 0x80) crc = (crc << 1) ^ 0x9c;
+			else crc = crc << 1;
+		}
+
+	if(buffer[byte] == '\r') bytes = byte + 3;
+	}
+
+	crc = (crc >> 2);
+	return (crc + 48);
+}
+
+unsigned char _checksum_validate(const char *buffer) {
+	//this function is also from the integrator guide
+	uint16_t length;
+	uint16_t i;
+	uint16_t sum = 0;
+
+	length = strlen(buffer);
+
+	for(i = 0; i < length; i++) {
+		sum += buffer[i];
+		if(buffer[i] == '\r') break;
+	}
+
+	sum += buffer[++i];
+	sum = sum % 64 + 32;
+
+	return sum;
+}
--- teros_11/teros_11.h
+++ teros_11/teros_11.h
@@ -1,20 +1,24 @@
 /*
- * pi pico library for the Teros 11 soil moisture sensor
- * uses "DDI Serial" (aka uart but they dont call it that)
- * because the other option is some weird standard that appears to just be rs232?
+ * teros_11.h
  *
- * 2023-05-29
+ * pi pico library for the teros 11/12 soil moisture sensor
+ * reads and decodes a 1200 baud serial signal from the sensor
+ *
+ * https://www.metergroup.com/en/meter-environment/products/teros-11-soil-moisture-sensor
+ *
+ * 2023-06-09
  * amelia vlahogiannis
  * amelia@ag2v.com
  */
 
-//serial port is 1200baud
 
 #ifndef TEROS_11_H
 #define TEROS_11_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "pico/stdlib.h"
 #include "pico/malloc.h"
 #define TEROS_BAUD_RATE 1200
@@ -23,6 +27,9 @@ typedef enum _return {
 	ok			=  0,
 	invalid_check_crc	= -1,
 	uart_not_enabled	= -2,
+	timed_out		= -3,
+	read_unsuccessful	= -4,
+	malloc_null		= -5,
 } teros_return;
 
 typedef enum _model {
@@ -49,9 +56,9 @@ typedef struct _data {
 	float vwc;
 	float temperature;
 	float conductivity;
-	uint8_t sensor_type;
-	uint8_t checksum;
-	uint8_t crc;
+	unsigned char sensor_type;
+	unsigned char checksum;
+	unsigned char crc;
 } teros_data;
 
 teros_return teros_init(
@@ -63,17 +70,11 @@ teros_return teros_init(
 		int pwr_pin,
 		teros_substrate substrate_type);
 teros_return teros_get_data(teros *teros, teros_data *data);
-teros_return teros_get_checksum(teros *teros, uint8_t *checksum);
-teros_return teros_get_crc(teros *teros, uint8_t *crc);
-teros_return teros_get_sensor_type(teros *teros, uint8_t *type);
-teros_return teros_get_vwc(teros *teros, float *vwc);
-teros_return teros_get_vwc_raw(teros *teros, float *vwc);
-teros_return teros_get_permittivity(teros *teros, float *permittivity);
-teros_return teros_get_tempc(teros *teros, float *temp);
 
-float raw_to_m3m3_mineral(float raw);
-float raw_to_m3m3_soilless(float raw);
+double _raw_to_m3m3_mineral(double raw);
+double _raw_to_m3m3_soilless(double raw);
 
-static float text_to_float(char *text);
+unsigned char _crc6_validate(const char *buffer);
+unsigned char _checksum_validate(const char *buffer);
 
 #endif 
