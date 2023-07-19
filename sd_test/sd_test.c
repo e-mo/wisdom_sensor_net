26ce9fa25594adc6401c5274a005bc5eb73477d9
amelia <amelia@ag2v.com>
2023-07-19 18:41:41 -0400

    mostly figured out the sd library i think

---
+++ sd_test/sd_test.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "hardware/dma.h"
+#include "hardware/timer.h"
+
+#include "ff.h"
+#include "f_util.h"
+
+#include "hw_config.h"
+
+// SPI Defines
+// We are going to use SPI 0, and allocate it to the following GPIO pins
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define SPI_PORT spi0
+#define PIN_MISO 20 
+#define PIN_CS   10
+#define PIN_SCK  18
+#define PIN_MOSI 19
+
+absolute_time_t on_time;
+
+
+
+int main()
+{
+    stdio_init_all();
+    
+    sleep_ms(5000);
+
+    sd_card_t *sd = sd_get_by_num(0);
+    FIL fil;
+    FRESULT fr;
+    UINT bw;
+    BYTE work[FF_MAX_SS];
+    char label[32];
+    const char* const filename = "sd_write_test.txt";
+
+    printf("starting\n");
+
+    fr = f_mount(&sd->fatfs, sd->pcName, 1);
+    if(fr != FR_OK) {
+	    printf("error mounting sd card!\n");
+	    printf("%s\n", FRESULT_str(fr));
+	    while(1);
+    }
+    else {
+	    printf("successfully mounted\n");
+	    //f_getlabel("", label, 0);
+	    //printf("volume label: %s\n", label);
+    }
+
+    fr = f_open(&fil, filename, FA_OPEN_APPEND | FA_WRITE);
+    if(fr != FR_OK && fr != FR_EXIST) {
+	    printf("error opening file!\n");
+	    printf("%s\n", FRESULT_str(fr));
+	    while(1);
+    }
+
+    if(f_printf(&fil, "mow\n")) {
+		    printf("error writing to file!\n");
+		    while(1);
+    }
+
+    f_close(&fil);
+    f_unmount(sd->pcName);
+
+    printf("complete");
+
+
+
+    return 0;
+}
