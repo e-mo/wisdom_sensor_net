6bf7dc4fc3aa02ef0fd167d68be6a72fc493d5b4
e-mo <emorse8686@gmail.com>
2024-02-27 16:52:54 -0500

    Post refactor radio test

--- rudp/src/rudp_test.c
+++ rudp/src/rudp_test.c
@@ -9,13 +9,25 @@
 
 int main() {
     stdio_init_all(); // To be able to use printf
-	
 
+
+	Rfm69 *rfm = rfm69_create();
+	Rfm69Config config = {
+		.spi      = spi0,
+		.pin_miso = RFM69_PIN_MISO,
+		.pin_cs   = RFM69_PIN_CS,
+		.pin_sck  = RFM69_PIN_SCK,
+		.pin_mosi = RFM69_PIN_MOSI,
+	    .pin_rst  = RFM69_PIN_RST
+	};
+
+	bool success = rfm69_init(rfm, &config);
+	
 	for(int i = 1;; i++) {
 		// Wait for USB serial connection
 		while (!tud_cdc_connected()) { sleep_ms(100); };
 
-		printf("%i: %i\n", i, RFM69_PIN_CS);
+		printf("%i: %i\n", i, success);
 		sleep_ms(1000);
 	}
     
