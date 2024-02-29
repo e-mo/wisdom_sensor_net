8c0c8a65bbed3bc6573d7f5e1b5d0cd3b18e9088
e-mo <emorse8686@gmail.com>
2024-02-29 09:18:50 -0500

    Comment rudp_init example

--- examples/rudp_init/src/rudp_init.c
+++ examples/rudp_init/src/rudp_init.c
@@ -16,11 +16,17 @@ int main() {
     stdio_init_all(); // To be able to use printf
 
 	bool success = false;
+	char *error = "success";
 
+	// Create rfm69 context
 	rfm69_context_t *rfm = rfm69_create();
-	if (rfm == NULL) goto LOOP_BEGIN;
+	if (rfm == NULL) {
+		error = "rfm69_create returned NULL";
+		goto LOOP_BEGIN;
+	}
 
-	// Pin definitions from winsdom_config.cmake
+	// Pull in definitions from winsdom_config.cmake
+	// spi0 is a macro defined in Pico API
 	rfm69_config_t config = {
 		.spi      = spi0,
 		.pin_miso = RFM69_PIN_MISO,
@@ -29,14 +35,22 @@ int main() {
 		.pin_mosi = RFM69_PIN_MOSI,
 	    .pin_rst  = RFM69_PIN_RST
 	};
-	if (rfm69_init(rfm, &config) == false)
+	// Initialize rfm69 context
+	if (rfm69_init(rfm, &config) == false) {
+		error = "rfm69_init failed";
 		goto LOOP_BEGIN;
-
+	}
+	// Create rudp context
 	rudp_context_t *rudp = rfm69_rudp_create();
-	if (rudp == NULL) goto LOOP_BEGIN;
-
-	if (rfm69_rudp_init(rudp, rfm) == false)
+	if (rudp == NULL) {
+		error = "rfm69_rudp_create returned NULL";
 		goto LOOP_BEGIN;
+	}
+	// Initialize rudp context
+	if (rfm69_rudp_init(rudp, rfm) == false) {
+		error = "rfm69_rudp_init failed";
+		goto LOOP_BEGIN;
+	}
 
 	success = true;	
 LOOP_BEGIN: 
@@ -45,7 +59,10 @@ LOOP_BEGIN:
 		// Wait for USB serial connection
 		while (!tud_cdc_connected()) { sleep_ms(100); };
 
-		printf("%i: success=%s\n", i, success ? "true" : "false");
+		// Print out how many times we have looped along with success variable
+		printf("%i:\tsuccess=%s\n", i, success ? "true" : "false");
+		// If success != true, also print error message
+		if (!success) printf("\t%s\n", error);
 
 		sleep_ms(1000);
 	}
