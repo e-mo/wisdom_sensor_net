e69ebcdc7f90e2d782c317a9aeaee40f0345b4b4
e-mo <emorse8686@gmail.com>
2024-03-06 17:36:32 -0500

    License new examples source

--- examples/rudp/rudp_init/src/rudp_init.c
+++ examples/rudp/rudp_init/src/rudp_init.c
@@ -5,6 +5,27 @@
 // Prints success=true to serial every second if init was successful
 // Prints success=false otherwise
 
+//	Copyright (C) 2024 
+//	Evan Morse
+//	Amelia Vlahogiannis
+//	Noelle Steil
+//	Jordan Allen
+//	Sam Colwell
+//	Rachel Cleminson
+
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include <stdio.h>
 
 #include "pico/stdlib.h"
