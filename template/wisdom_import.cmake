c173bc4c2a37a0436b0d94b4e9444c0995fde02a
e-mo <emorse8686@gmail.com>
2024-03-14 06:54:51 -0400

    Stop copying libraries in build process

--- template/wisdom_import.cmake
+++ template/wisdom_import.cmake
@@ -4,14 +4,12 @@ set(WISDOM_PROJECT_PATH ${WISDOM_PROJECT_PATH} CACHE PATH "Root of Wisdom Repo" 
 
 # RFM69
 message("wisdom_init: building rfm69_pico library")
-file(COPY ${WISDOM_PROJECT_PATH}/rfm69 DESTINATION ${CMAKE_BINARY_DIR})
-add_subdirectory(${CMAKE_BINARY_DIR}/rfm69)
+#file(COPY ${WISDOM_PROJECT_PATH}/rfm69 DESTINATION ${CMAKE_BINARY_DIR})
+add_subdirectory(${WISDOM_PROJECT_PATH}/rfm69 rfm69)
 
 # FatFs_SPI
 message("wisdom_init: building FatFs_SPI library")
-file(COPY ${WISDOM_PROJECT_PATH}/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI 
-	DESTINATION ${CMAKE_BINARY_DIR})
-add_subdirectory(${CMAKE_BINARY_DIR}/FatFs_SPI)
+add_subdirectory(${WISDOM_PROJECT_PATH}/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI FatFs_SPI)
 
 # Load local config
 message("wisdom_init: loading local wisdom_config.cmake file")
