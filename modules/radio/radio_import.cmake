ffc78b291f4791faa53d7fa653ec04db9e5165c1
e-mo <emorse8686@gmail.com>
2024-03-14 20:37:24 -0400

    Create driver path cmake var

--- modules/radio/radio_import.cmake
+++ modules/radio/radio_import.cmake
@@ -1,10 +1,11 @@
 set(WISDOM_PROJECT_PATH "~/pico/wisdom_sensor_net")
 get_filename_component(WISDOM_PROJECT_PATH "${WISDOM_PROJECT_PATH}" REALPATH BASE_DIR "${CMAKE_BINARY_DIR}")
 set(WISDOM_PROJECT_PATH ${WISDOM_PROJECT_PATH} CACHE PATH "Root of Wisdom Repo" FORCE)
+set(WISDOM_DRIVERS_PATH "${WISDOM_PROJECT_PATH}/drivers")
 
 # RFM69
 message("wisdom_init: building rfm69_pico library")
-add_subdirectory(${WISDOM_PROJECT_PATH}/rfm69 libs)
+add_subdirectory(${WISDOM_DRIVERS_PATH}/rfm69_rp2040 rfm69_rp2040)
 
 # Load local config
 message("wisdom_init: loading local radio_config.cmake file")
