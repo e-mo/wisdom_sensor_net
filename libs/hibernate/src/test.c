a02ee2f8a1e5050cb95bc810bbf5a292347c1e34
Evan Morse <emorse8686@gmail.com>
2024-09-10 18:47:59 -0400

    Comment hibernate test

--- libs/hibernate/src/test.c
+++ libs/hibernate/src/test.c
@@ -2,14 +2,27 @@
 #include "pico/stdlib.h"
 
 #include "hibernate.h"
+#include "pcf8523_rp2040.h"
 
 int main() {
 	stdio_init_all();
 
+	// Setup RTC to send interrupt soon
+
+	// Save old clocks
+	uint clock0_orig = 0;
+	uint clock1_orig = 0;
+
+	// Configure clocks to go dormant
 	hibernate_run_from_dormant_source(DORMANT_SOURCE_XOSC);
 
+	// Go dormant until falling edge of active low signal
 	hibernate_goto_dormant_until_pin(2, false, false);
 
+	// Recover clocks after hibernation
+	hibernate_recover_clocks(clock0_orig, clock1_orig);
+
+	// Attempt to print after waking up (proving USB serial is OK)
 	for (;;) {
 
 		printf("hello!\n");
